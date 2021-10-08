2708fa787daa9cd9f82014aead95d8e6cc2f658d
Grigory Kozlov <Grigory.Kozlov@itechart-group.com>
2021-10-08 14:06:01 +0300

    feat: add pagination

--- packages/apps/multikart/src/plugins/components/a-product-listing/index.js
+++ packages/apps/multikart/src/plugins/components/a-product-listing/index.js
@@ -18,7 +18,7 @@ const AProductListing = (props, {contextId, pageId, pageType, searchServicePath}
   const {regions = [], configuration = {}} = props;
   const {className = ''} = configuration || {};
 
-  const [searchParams, setSearchParams] = useState({N: '', Ns: '', No: '0', Nrpp: '25'});
+  const [searchParams, setSearchParams] = useState({N: '', Ns: '', No: '0', Nrpp: '15'});
 
   const globalChangeHandler = () => {
     if (searchParams) {
--- packages/apps/multikart/src/plugins/components/product-pagination/locales/en.js
+++ packages/apps/multikart/src/plugins/components/product-pagination/locales/en.js
@@ -1,5 +1,5 @@
 export const helloText = 'sample component text';
 export const labelExtraInfo = '...with some extra info!';
 export const labelSampleTranslation = 'A Translated Label';
-export const previous = 'Previous';
+export const previous = 'Prev';
 export const next = 'Next';
