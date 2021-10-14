dfd3fd39ecc664c3ef0364545023536e7605c2b3
Grigory Kozlov <Grigory.Kozlov@itechart-group.com>
2021-10-14 18:40:23 +0300

    feat: query string

--- packages/apps/multikart/src/plugins/fetchers/a-product-listing/fetch-search-results/index.js
+++ packages/apps/multikart/src/plugins/fetchers/a-product-listing/fetch-search-results/index.js
@@ -11,7 +11,6 @@ import {getSearchResultsFetcherData} from '@oracle-cx-commerce/fetchers/search/s
 export const fetchSearchResults = async (store, {searchServicePath}) => {
   const {contextId, pageId, pageType} = getSearchResultsFetcherData(store.getState());
   const payload = {url: pageId, searchServicePath};
-
   /**
    * FIXME: TEMPORARY: If this fetcher is for a category page, look up the
    * dimension id for the category and add it to the payload. Ideally, GS would
@@ -26,6 +25,18 @@ export const fetchSearchResults = async (store, {searchServicePath}) => {
       const category = getCategories(state)[categoryId];
       if (category && category.dimensionId) {
         payload.dimensionId = category.dimensionId;
+        const getQuery = new URLSearchParams(payload.url.slice(payload.url.indexOf('?') + 1));
+
+        const urlObj = {
+          N: getQuery.get('N') ? getQuery.get('N') : payload.dimensionId,
+          Ns: getQuery.get('Ns') || '',
+          No: getQuery.get('No') || '0',
+          Nrpp: getQuery.get('Nrpp') || '10'
+        };
+        console.log(urlObj);
+
+        payload.url += `?N=${urlObj.N}&$Ns=${urlObj.Ns}&No=${urlObj.No}&Nrpp=${urlObj.Nrpp}`;
+        console.log(payload.url);
       }
     }
   }
